25deb8fed4623f25ef21f7ee300e65b921426285
akoeth-portfolio <dukewellington61@googlemail.com>
2020-04-16 00:00:08 +0200

    1:32

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/ToDos';
 import AddToDo from './components/AddToDo';
-
+import About from './components/pages/About';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 import './App.css';
 
+
 class App extends Component {
   state = {
-    todos: [
-      { 
-        id: 1,
-        title: 'Drink beer.',
-        completed: false
-      },
-      { 
-        id: 2,
-        title: 'Drink more beer.',
-        completed: false
-      },
-      { 
-        id: 3,
-        title: 'Drink even more beer',
-        completed: false
-      }
-    ]
+    todos: []
+  }
+
+  componentDidMount() {
+     axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+     .then(res => this.setState( { todos: res.data }));
   }
 
   // Toggle Complete
@@ -37,19 +30,52 @@ class App extends Component {
     })})
   }
 
+  // Delete ToDo
   delToDo = id => {
     this.setState({ todos: this.state.todos.filter(todo => todo.id !== id) })
   }
 
+  addToDo = title => {
+    const newToDo = {
+      id: uuidv4(),
+      title,
+      completed: false
+    }
+
+    this.setState({ todos: [...this.state.todos, newToDo]})
+  }
+
+  // This version of the addToDo function produces issues because the id of every added object is the same 
+  // causing the same children / unique key blah blah error
+  // Add ToDo
+  // addToDo = title => {
+  //   const proxy = 'https://cors-anywhere.herokuapp.com/'; /* add proxy to avoid cors issues */
+  //   axios.post(`${proxy}https://jsonplaceholder.typicode.com/todos`, {
+  //     title,
+  //     completed: false
+  //   })
+  //     .then(res => this.setState({ todos: 
+  //     [...this.state.todos, res.data] }));
+  // }
+
+    
+
   render() {    
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddToDo />
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delToDo={this.delToDo}/>
-        </div>       
-      </div>
+      <Router>    
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddToDo addToDo={this.addToDo}/>
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delToDo={this.delToDo}/>
+              </React.Fragment>
+            )} />  
+            <Route path="/about" component={About} />     
+          </div>       
+        </div>
+      </Router>
     );
   }
 } 
--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react'
 
 export class AddToDo extends Component {
+    state = {
+        title: ''
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addToDo(this.state.title);
+        this.setState({ title: '' });
+    }
+
+    onChange = e => this.setState({ title: e.target.value });
+
+    // In case we have more than one input field e.g. name, age, whatever, .. 
+    // [e.target.name] is the key title in the state object (if attribute name="title" is set in input tag)
+    // onChange = e => this.setState({ [e.target.name]: e.target.value });
+
     render() {
         return (
-           <form style={{ display: 'flex' }}>
+           <form onSubmit={this.onSubmit} style={{ display: 'flex' }}>
                <input 
                     style={{ flex: '10', padding: '5px' }}
                     type="text"
                     name="title"
                     placeholder="Add Todo ..."
-                    required
+                    value={this.state.title}
+                    onChange={this.onChange}
                 />
                 <input 
                     type="submit" 
                     value="Submit"
                     className="btn"
-                    style={{flex: '1'}}
+                    style={{flex: '1'}}                    
                 />
            </form>
         )
